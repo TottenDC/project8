40bc864234fdbeae78802a76a0a82e70e5c0d6c7
Daniel Totten <TottenDC@gmail.com>
2019-01-06 08:16:50 -0500

    Sets up styles task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const del = require('del');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
 
 
 //Tasks
@@ -26,8 +28,20 @@ function minifyJS() {
          .pipe(dest('dist/scripts'));
 }
 
-function styles() {
+function compileSass() {
+  return src('sass/global.scss')
+         .pipe(rename('all.css'))
+         .pipe(maps.init())
+         .pipe(sass())
+         .pipe(maps.write('../dist/styles'))
+         .pipe(dest('tmp'));
+}
 
+function minifySass() {
+  return src('tmp/all.css')
+         .pipe(csso())
+         .pipe(rename('all.min.css'))
+         .pipe(dest('dist/styles'));
 }
 
 function images() {
@@ -42,12 +56,13 @@ function clean() {
 exports.scripts = series(
   concatJS, minifyJS
 );
-exports.styles = styles;
+exports.styles = series(compileSass, minifySass);
 exports.images = images;
 exports.clean = clean;
 exports.build = series(
   clean,
   parallel(
     series(concatJS, minifyJS),
-    styles, images)
+    series(compileSass, minifySass),
+    images)
 );
